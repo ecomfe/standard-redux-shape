48e9aaf632cd141d1c287797920dc0b66397c8c2
otakustay <otakustay@gmail.com>
2017-08-30 14:19:13 +0800

    添加Table的初始化逻辑

--- src/entities.js
+++ src/entities.js
@@ -5,7 +5,7 @@
 
 import {immutable} from 'san-update';
 
-const UPDATE_ENTITY_TABLE = 'UPDATE_ENTITY_TABLE';
+const UPDATE_ENTITY_TABLE = '@@standard-redux-shape/UPDATE_ENTITY_TABLE';
 
 const isEmpty = o => {
     for (const key in o) {
@@ -28,18 +28,18 @@ const reduce = (object, iteratee, initialValue) => {
  * 当传递一个`store`后，会返回一个`withTableUpdate`函数，这个函数的用法如下：
  *
  * ```javascript
- * const apiWithTableUpdate = withTableUpdate(entitySetName, selectEntities)(api);
+ * const apiWithTableUpdate = withTableUpdate(tableName, selectEntities)(api);
  * const response = await apiWithTableUpdate(args);
  * ```
  *
  * 当一个用于获取后端数据的API函数被这个高阶函数包装后，会在响应返回时额外做以下行为：
  *
  * 1. 调用`selectEntities`函数，将`response`传入并得到一个对象，该对象是一系列需要更新的实体，以实体的索引属性（比如id）为键
- * 2. 派发一个类型为`UPDATE_ENTITY_TABLE`的Action，并通过`payload`提供`entitySetName`和`entity`属性
+ * 2. 派发一个类型为`UPDATE_ENTITY_TABLE`的Action，并通过`payload`提供`tableName`和`entity`属性
  *
  * 当reducer包含了对这个Action的处理时，会有以下逻辑：
  *
- * 1. 根据`entitySetName`从`state`中获取到对应的表
+ * 1. 根据`tableName`从`state`中获取到对应的表
  * 2. 将当前从响应中获取的实体一一合并到表中
  *
  * 通过这种方式，可以将后端的接口与Normalized Store中的实体信息建立关联，保持所有实体更新在实体表中，其它地方通过id的方式引用保持信息同步
@@ -56,10 +56,10 @@ const reduce = (object, iteratee, initialValue) => {
  *
  * @param {Object} store Redux store
  */
-export const createTableUpdater = ({dispatch}) => (entitySetName, selectEntities) => {
+export const createTableUpdater = ({dispatch}) => (tableName, selectEntities) => {
     const dispatchTableUpdate = (responseData, ...args) => {
         const entities = selectEntities(responseData, ...args);
-        dispatch({type: UPDATE_ENTITY_TABLE, payload: {entitySetName, entities}});
+        dispatch({type: UPDATE_ENTITY_TABLE, payload: {tableName, entities}});
         return responseData;
     };
 
@@ -76,13 +76,22 @@ export const createTableUpdateReducer = (nextReducer = s => s) => (state = {}, a
         return nextReducer(state, action);
     }
 
-    const {payload: {entitySetName, entities}} = action;
-    const entitySet = state[entitySetName] || {};
+    const {payload: {tableName, entities}} = action;
+
+    // 在第一次调用`withTableUpdate`的时候，会触发对当前表的初始化，
+    // 当然如果一个表对应多个API的话，初始化会触发多次，在按需加载的时候也可能在任意时刻触发（通过`replaceReducer`），
+    // 所以当表不存在的时候才进行赋值
+    if (!entities && !state[tableName]) {
+        return {...state, [tableName]: {}};
+    }
+
+    const table = state[tableName] || {};
+
     // 因为当前的系统前后端接口并不一定会返回一个完整的实体，
     // 如果之前有一个比较完整的实体已经在`state`中，后来又来了一个不完整的实体，直接覆盖会导致字段丢失，
     // 因此针对每个实体，使用的是浅合并的策略，保证第一级的字段是不会丢的
-    const merging = reduce(entities, (chain, value, key) => chain.merge(key, value), immutable(entitySet));
-    const [mergedEntitySet, diff] = merging.withDiff();
-    const newState =  isEmpty(diff) ? state : {...state, [entitySetName]: mergedEntitySet};
+    const merging = reduce(entities, (chain, value, key) => chain.merge(key, value), immutable(table));
+    const [mergedTable, diff] = merging.withDiff();
+    const newState =  isEmpty(diff) ? state : {...state, [tableName]: mergedTable};
     return nextReducer(newState, action);
 };
